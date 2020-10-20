64b95bf67b402ccbbf0a8baa34af7ae432eedd67
cycampos <ccampos@chainbridgesolutions.com>
2020-10-20 14:38:44 -0400

    added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  function callRest() {
+    console.log("calling rest");
+
+    fetch("https://aav45kv0rd.execute-api.us-east-1.amazonaws.com/Prod/hello")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          console.log(result);
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          console.log("getLocation error", error);
+        }
+      );
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Click this button to call rest</p>
+        <button onClick={callRest}>Click Me</button>
+        <textarea id="return"></textarea>
       </header>
     </div>
   );
